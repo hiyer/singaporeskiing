0ed04eb72d4d74829683da68177e63b37f373689
Hariharan Iyer <hiyer@localhost.localdomain>
2018-04-23 20:07:42 +0530

    fixes

--- skiing.py
+++ skiing.py
@@ -1,5 +1,4 @@
 #!/home/hiyer/.venv3/bin/python
-
 from functools import total_ordering
 
 @total_ordering
@@ -26,6 +25,13 @@ class PathPoint:
         self._length += 1
         self._elevation += elevation
         
+    def is_valid(self):
+        return self._length >= 0
+        
+
+def neighbours(x, y):
+    return [(x-1, y), (x, y-1), (x+1, y), (x, y+1)]
+        
 lines = []
 with open("/home/hiyer/Downloads/map.txt") as f:
     for line in f:
@@ -34,52 +40,58 @@ with open("/home/hiyer/Downloads/map.txt") as f:
 (num_cols, num_rows) = lines.pop(0)
 
 path_points = []
-for j in range(num_cols):
+for j in range(num_rows):
     row = []
-    for k in range(num_rows):
-        row.append(PathPoint(0, 0))
-        
+    for k in range(num_cols):
+        row.append(PathPoint(-1, -1))
     path_points.append(row)
     
-def neighbours(x, y):
-    return [(x-1, y), (x, y-1), (x+1, y), (x, y+1)]
-        
-def best_ending_at(x, y):
-    elevation = lines[x][y]
-    best_elevation = 0
-    best_point = None
-    best_path_point = None
+def find_longest_path(x, y):
+    elevation = lines[y][x]
+    longest_path = None
+    
+    #print("Finding longest path from {}, {} at elevation {}".format(x, y, elevation))
+    
+    if path_points[y][x].is_valid():
+        return path_points[y][x]
+    
     for j, k in neighbours(x, y):
-        if j < 0 or j > (num_cols - 1) or k < 0 or (k > num_rows - 1):
+        if j < 0 or j > num_cols - 1 or k < 0 or k > num_rows - 1:
             continue
-
-        #print("Evaluating {}, {}, elevation {}".format(j, k, elevation))
-        if lines[j][k] > elevation:
-            #print("Evaluating {}, {}, difference in elevation {}".format(j, k, lines[j][k] - elevation))
-            path_point = path_points[j][k]
-            candidate_point = PathPoint(path_point._length, path_point._elevation)
-            #print("Path point is {}, {}".format(path_point._length, path_point._elevation))
-            candidate_point.add(lines[j][k] - elevation)
-            #print("Candidate point is {}, {}".format(candidate_point._length, candidate_point._elevation))
-            if best_path_point is None or best_path_point < candidate_point:
-                best_path_point = candidate_point
                 
-    if best_path_point is None:
-        path_points[x][y] = PathPoint(0, 0)
-        #print("No paths to {}, {}".format(x, y))
+        if lines[k][j] < elevation:
+            path = find_longest_path(j, k)
+                
+            if path is None:
+                continue
+                
+            if longest_path is None or longest_path < path:
+                tmp_path = PathPoint(path_points[k][j]._length, path_points[k][j]._elevation)
+                tmp_path.add(elevation - lines[k][j])
+                longest_path = tmp_path
+    
+    if longest_path is not None:
+        path_points[y][x] = longest_path
+        #print("Longest path from {}, {} is {}, {}".format(x, y, longest_path._length, longest_path._elevation))
     else:
-        path_points[x][y] = best_path_point
-        #print("Best for {}, {} is {}, {}".format(x, y, best_path_point._length, best_path_point._elevation))
+        longest_path = PathPoint(0, 0)
+        path_points[y][x] = longest_path
+        #print("No path from {}, {}".format(x, y))
         
-    return best_path_point
+    return longest_path
 
-
-best_overall = None
-for x, line in enumerate(lines):
-    for y, point in enumerate(line):
-        best_for_point = best_ending_at(x, y)
+longest_path = None
+for y, line in enumerate(lines):
+    for x, point in enumerate(line):
+        path = find_longest_path(x, y)
         
-        if best_for_point is not None and (best_overall is None or best_overall < best_for_point):
-            best_overall = best_for_point
-            
-print("Best overall is {}, {}".format(best_overall._length+1, best_overall._elevation))
+        if path is None:
+            continue
+        
+        if path is not None and (longest_path is None or longest_path < path):
+            longest_path = path
+
+if longest_path is None:
+    print("No path found")
+else:
+    print("Longest path is {}, {}".format(longest_path._length+1, longest_path._elevation))
