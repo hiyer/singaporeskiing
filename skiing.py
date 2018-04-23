58bc94655661bff1ec541fd92b9867408b7a3959
Hariharan Iyer <hiyer@localhost.localdomain>
2018-04-24 00:31:45 +0530

    fixes

--- skiing.py
+++ skiing.py
@@ -1,5 +1,7 @@
 #!/home/hiyer/.venv3/bin/python
+
 from functools import total_ordering
+import sys
 
 @total_ordering
 class PathPoint:
@@ -31,9 +33,13 @@ class PathPoint:
 
 def neighbours(x, y):
     return [(x-1, y), (x, y-1), (x+1, y), (x, y+1)]
-        
+
+if len(sys.argv) != 2:
+    print("Usage: skiing.py <filename>")
+    sys.exit(1)
+    
 lines = []
-with open("/home/hiyer/Downloads/map.txt") as f:
+with open(sys.argv[1]) as f:
     for line in f:
         lines.append([int(x) for x in line.split()])
 
@@ -48,37 +54,34 @@ for j in range(num_rows):
     
 def find_longest_path(x, y):
     elevation = lines[y][x]
-    longest_path = None
-    
-    #print("Finding longest path from {}, {} at elevation {}".format(x, y, elevation))
+    longest_subpath = None
+    next_point = None
     
     if path_points[y][x].is_valid():
         return path_points[y][x]
-    
-    for j, k in neighbours(x, y):
-        if j < 0 or j > num_cols - 1 or k < 0 or k > num_rows - 1:
+
+    for (j, k) in neighbours(x, y):
+        if j < 0 or j > num_cols - 1 or k < 0 or k > num_rows - 1 or lines[k][j] >= elevation:
             continue
+        
+        #print("Looking in sub-path {}, {} for {}, {}".format(j, k, x, y))
+        subpath = find_longest_path(j, k)
+        tmp_path = PathPoint(subpath._length, subpath._elevation)
+        tmp_path.add(elevation - lines[k][j])
+        
+        if longest_subpath is None or longest_subpath < tmp_path:
+            longest_subpath = tmp_path
+            #print("Longest sub-path: {}, {} at {}, {}".format(longest_path._length, longest_path._elevation, x, y))
                 
-        if lines[k][j] < elevation:
-            path = find_longest_path(j, k)
-                
-            if path is None:
-                continue
-                
-            if longest_path is None or longest_path < path:
-                tmp_path = PathPoint(path_points[k][j]._length, path_points[k][j]._elevation)
-                tmp_path.add(elevation - lines[k][j])
-                longest_path = tmp_path
-    
-    if longest_path is not None:
-        path_points[y][x] = longest_path
-        #print("Longest path from {}, {} is {}, {}".format(x, y, longest_path._length, longest_path._elevation))
+    if longest_subpath is not None:
+        path_points[y][x] = longest_subpath
+        #print("Longest path from {}, {} is {}, {} to {}, {}".format(x, y, longest_subpath._length, longest_subpath._elevation, next_point._x, next_point._y))
     else:
-        longest_path = PathPoint(0, 0)
-        path_points[y][x] = longest_path
+        longest_subpath = PathPoint(0, 0)
+        path_points[y][x] = longest_subpath
         #print("No path from {}, {}".format(x, y))
         
-    return longest_path
+    return longest_subpath
 
 longest_path = None
 for y, line in enumerate(lines):
